ea7f32de838e9d5d8d4a8c4f78c9de55aa27c5cd
udaysai tejasri komanduri <u.komanduri@dxc.com>
2020-09-28 11:32:47 +0530

    Added Product Controller

---
+++ Bakery/Controllers/ProductAdd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bakery.Controllers
+{
+    public class ProductAdd : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Food()
+        {
+            return View();
+        }
+    }
+}
